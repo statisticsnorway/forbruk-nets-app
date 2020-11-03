88d821fafd91db1c6079883a3737393c03ee4268
rsassb <rsa@ssb.no>
2020-11-03 13:50:20 +0100

    Remove scheduling and create controller instead.

---
+++ src/main/java/no/ssb/forbruk/nets/controller/NetsController.java
@@ -0,0 +1,30 @@
+package no.ssb.forbruk.nets.controller;
+
+import no.ssb.forbruk.nets.sftp.SftpFileTransfer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class NetsController {
+    private static final Logger logger = LoggerFactory.getLogger(NetsController.class);
+
+    @Autowired
+    SftpFileTransfer sftpFileTransfer;
+
+
+    @GetMapping("/netsfiles")
+    public ResponseEntity<String> runSftpFileTransferList() {
+        logger.info("Called netsfiles - " + LocalDateTime.now());
+        sftpFileTransfer.list();
+        return new ResponseEntity<>("Files treated", HttpStatus.OK);
+
+    }
+}
+
