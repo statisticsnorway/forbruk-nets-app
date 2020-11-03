eb9dc82c3ac33db7287f825949279ea0559df822
rsassb <rsa@ssb.no>
2020-11-03 14:17:42 +0100

    Remove scheduling and create controller instead.

--- src/main/java/no/ssb/forbruk/nets/scheduling/ScheduledTasks.java
+++ src/main/java/no/ssb/forbruk/nets/scheduling/ScheduledTasks.java
@@ -4,7 +4,7 @@ import no.ssb.forbruk.nets.sftp.SftpFileTransfer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +14,7 @@ public class ScheduledTasks {
     @Autowired
     SftpFileTransfer sftpFileTransfer;
 
-    @Scheduled(cron = "${scheduled.cron.listfiles}")
+//    @Scheduled(cron = "${scheduled.cron.listfiles}")
     public void runSftpFileTransferList() {
         logger.info("schedule task - " + System.currentTimeMillis() / 1000);
         sftpFileTransfer.list();
