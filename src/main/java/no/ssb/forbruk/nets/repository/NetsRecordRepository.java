13f0753def56c767c147f3f813d1d3a2733cacee
rsassb <rsa@ssb.no>
2020-11-05 15:58:46 +0100

    Add spring-boot-starter-web, remove spring-boot-starter-data-rest (#7)

--- src/main/java/no/ssb/forbruk/nets/repository/NetsRecordRepository.java
+++ src/main/java/no/ssb/forbruk/nets/repository/NetsRecordRepository.java
@@ -1,12 +1,13 @@
 package no.ssb.forbruk.nets.repository;
 
 import no.ssb.forbruk.nets.model.NetsRecord;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "netsRecord", path = "netsRecord")
-public interface NetsRecordRepository extends CrudRepository<NetsRecord, Long> {
+//@RepositoryRestResource(collectionResourceRel = "netsRecord", path = "netsRecord")
+public interface NetsRecordRepository extends JpaRepository<NetsRecord, Long> {
 
 }
