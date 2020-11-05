8f0aefd67850d372c565549b1235a5b5176e9fbb
rsassb <rsa@ssb.no>
2020-11-05 09:11:30 +0100

    Use Environment-variables (#5)
    * Remove scheduling and create controller instead.
    * Use Environment-variables

--- src/main/java/no/ssb/forbruk/nets/health/ReadinessService.java
+++ src/main/java/no/ssb/forbruk/nets/health/ReadinessService.java
@@ -1,18 +1,18 @@
 package no.ssb.forbruk.nets.health;
 
+import no.ssb.forbruk.nets.sftp.SftpFileTransfer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import no.ssb.forbruk.nets.service.AppSecretsService;
 
 @Service
 public class ReadinessService {
 
-    private AppSecretsService appSecretsService;
+    private SftpFileTransfer sftpFileTransfer;
 
     @Autowired
-    public ReadinessService(AppSecretsService appSecretsService){this.appSecretsService = appSecretsService;}
+    public ReadinessService(SftpFileTransfer sftpFileTransfer){this.sftpFileTransfer = sftpFileTransfer;}
 
     private static final Logger logger = LoggerFactory.getLogger(ReadinessService.class);
 
@@ -20,11 +20,7 @@ public class ReadinessService {
         // This might not be best practice, since no traffic gets routed to the service, but it depends on
         // your service. This might lead to cascading errors in other services, and one should consider
         // queueing requests.
-        return isSecretAvailable();
+        return sftpFileTransfer.secretsOk();
     }
 
-    private boolean isSecretAvailable() {
-        String secret = appSecretsService.getNetsSecret();
-        return secret != null && secret.length() != 0;
-    }
 }
