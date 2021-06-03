982d322f286634f48e57bb3cb8f1c99ccf4e6cb2
fnh-ssb <fnh@ssb.no>
2021-06-03 02:40:40 +0200

    udpate

--- src/test/java/no/ssb/forbruk/nets/MemoryMachineTest.java
+++ src/test/java/no/ssb/forbruk/nets/MemoryMachineTest.java
@@ -18,13 +18,21 @@ public class MemoryMachineTest {
             String line1 = myReader.nextLine();
             do {
                 String data = myReader.nextLine();
-                if (!data.startsWith("01/02")) {
-
-                } else {
+                if (line1.startsWith("01/02") && !data.startsWith("01/02")) {
                     System.out.println(line1+data);
+                } else if (line1.startsWith("01/02") && data.startsWith("01/02")){
+
+                    System.out.println(line1);
+                } else {
+                    //DO nothing
                 }
+                line1=data;
 
             } while(myReader.hasNextLine());
+
+            // process last line
+            System.out.println(line1);
+
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
